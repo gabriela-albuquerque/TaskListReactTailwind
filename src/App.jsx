7e2c5c724a9d84a64357529ee2f9dfaaef59131c
Gabriela Albuquerque <126294239+gabriela-albuquerque@users.noreply.github.com>
2025-07-17 11:45:38 -0300

    implementada funacao que atualiza o estado de conclusao da tarefa e atualiza a mesma incluindo a estilizacao de texto riscado

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,19 @@ function App() {
     },
   ]);
 
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      // PRECISO ATUALIZAR ESSA TAREFA
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      // NÃO PRECISO ATUALIZAR ESSA TAREFA
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
@@ -33,7 +46,7 @@ function App() {
           Gerenciador de Tarefas
         </h1>
         <AddTasks />
-        <Tasks tasks={tasks} />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
       </div>
     </div>
   );
