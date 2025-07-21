3cef8cb09ec4d9d09380b8efff1ec336cae9ba0f
Gabriela Albuquerque <126294239+gabriela-albuquerque@users.noreply.github.com>
2025-07-21 10:00:17 -0300

    componentizacao do Input

--- src/components/AddTasks.jsx
+++ src/components/AddTasks.jsx
@@ -1,21 +1,20 @@
 import { useState } from "react";
+import Input from "./Input";
 
 function AddTasks({ onAddTaskSubmit }) {
   const [title, setTitle] = useState("");
   const [description, setDescripition] = useState("");
   return (
     <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
-      <input
+      <Input
         type="text"
         placeholder="Digite o título da tarefa"
-        className="border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
         value={title}
         onChange={(event) => setTitle(event.target.value)}
       />
-      <input
+      <Input
         type="text"
         placeholder="Digite a descrição da tarefa"
-        className="border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
         value={description}
         onChange={(event) => setDescripition(event.target.value)}
       />
